15099607dc25f769daad2a597229d1e85c01ffba
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-10-03 12:43:28 -0400

    start
    erjyj

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+    public float horizontalInput;
+    public float speed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
