7d3872e36a78e542c2306c326b2d517d2404f665
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-10-04 12:37:02 -0400

    2.1 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,24 @@ public class PlayerController : MonoBehaviour
         
     }
     public float horizontalInput;
-    public float speed = 10.0f;
+    public float xRange = 10;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
+        //keep the player in bounds
+        if (transform.position.x < xRange)
+        { 
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x >xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
