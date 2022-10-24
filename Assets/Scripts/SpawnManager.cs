1b034a9edb00ed2775989f8ac08789a210f681cb
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-10-24 12:05:36 -0400

    2.4 Complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +20,14 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-            //randomly generate animal index and spawn position//
-            SpawnRandomAnimal ();
-
-            void SpawnRandomAnimal() {
-                int animalIndex = Random.Range(0, animalPrefabs.Length);
-                Vector3 spawnos = new Vector3(Random.Range (-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-                 Instantiate(animalPrefabs[animalIndex], new = Vector3(0, 0 , 20) spawnpos, animalPrefabs[animalIndex].transform.rotation);
-            }
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-                int animalIndex = Random.Range(0,animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+         
     }
+
+        void SpawnRandomAnimal() 
+        {
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+                Vector3 spawnpos = new Vector3(Random.Range (-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+                //randomly generate animal index and spawn position//
+                 Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
+        }
 }
